8937d741abb42482f50261d52725d2144fb56a26
vishmantha <vishmantha.b@eyepax.com>
2018-10-02 18:31:35 +0530

    first commit - carousel , navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {SlideshowModule} from 'ng-simple-slideshow';
+import { CarouselModule } from 'ngx-bootstrap';
+
 
 import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CarouselModule.forRoot()
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
