ccabee05d4488a6474536c5f82212832762e959d
vishmantha <vishmantha.b@eyepax.com>
2018-10-03 15:08:00 +0530

    upto navbar search bar in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {SlideshowModule} from 'ng-simple-slideshow';
 import { CarouselModule } from 'ngx-bootstrap';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationBarComponent,
+    SearchBarComponent,
   ],
   imports: [
     BrowserModule,
+    [NgSelectModule, FormsModule],
     CarouselModule.forRoot()
     
   ],
